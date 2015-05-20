e78b80c5eb3fe6dfb9c5c91499a3a3176182a7f7
meni <meni@gmail.com>
2015-05-20 16:38:22 +0300

    6

---
+++ Lab 6/Task 0/src/task2.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include "LineParser.h"
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+typedef struct record
+{
+    cmdLine* line;
+    int index;
+    struct record* next;
+} record;
+
+int PATH_MAX = 100; /*can I delete this?*/
+record* history = NULL;
+
+void printHistory(record * curr) {
+    if (curr != NULL) {
+        printHistory(curr->next);
+        int i;
+        for (i = 0; i < curr->line->argCount; i++)
+            printf("%s ", curr->line->arguments[i]);
+        printf("\n");
+    }
+}
+
+cmdLine* callPast(record* past, int i) {
+    record* curr = past;
+    if (curr->index == i)
+        return curr->line;
+    return callPast(past->next,i);
+}
+
+void freeAll(record * curr) {
+    if (curr != NULL) {
+        freeAll(curr->next);
+        freeCmdLines(curr->line); 
+        free(curr);      
+    }
+}
+
+void execute(cmdLine *pCmdLine) {
+    pid_t pid;
+    int i;
+    if (strcmp(pCmdLine->arguments[0],"quit") == 0) {
+      freeAll(history);
+      _exit(1);  
+    }
+    if (strcmp(pCmdLine->arguments[0],"cd") == 0) {
+        int err = chdir(pCmdLine->arguments[1]);
+        if (err) {
+            perror("problem in directory");
+        }
+    }
+    if (strcmp(pCmdLine->arguments[0],"history") == 0) {
+        printHistory(history);
+    }
+    else if(!( pid = fork() )) {
+        execvp(pCmdLine->arguments[0],pCmdLine->arguments);
+        perror("An error has occurred");
+        freeAll(history);
+        _exit(1);
+    }
+    if (pCmdLine->blocking == 1) {
+        waitpid(pid,&i,0);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int largeIndex;
+    while(1) {
+        largeIndex = 0;
+        record* newRec = malloc(sizeof(record));
+        char* cwd;
+        char buff[PATH_MAX + 1];
+        char input[PATH_MAX + 1];
+        cwd = getcwd(buff, PATH_MAX + 1);
+        if( cwd != NULL ) {
+            printf("%s>", cwd );
+        }
+        fgets(input,PATH_MAX,stdin);
+        /*if (input != NULL)
+            printf("input line is : %s", input);*/
+        cmdLine* newCmd = parseCmdLines(input);
+        if (newCmd->arguments[0][0] == '!') {
+            if (history != NULL) {
+                int callIndex = newCmd->arguments[0][1] - '0';
+                if (callIndex > history->index) {
+                    printf("Index out of bound\n");
+                    largeIndex = 1;
+                }
+                else
+                    newCmd = callPast(history,callIndex);
+                }
+            else {
+                printf("Index out of bound\n");
+                largeIndex = 1;
+            }
+        }
+        newRec->line = newCmd;
+        if (history == NULL)
+            newRec->index = 0;
+        else
+            newRec->index = (history->index) + 1;
+        newRec->next = history;
+        history = newRec;
+        /*if (newCmd != NULL) {
+            printf("command name is : %s\n", newCmd->arguments[0]);
+        }*/
+        if (!largeIndex) 
+            execute(newCmd);
+    }
+    freeAll(history);
+    return 0;
+}
